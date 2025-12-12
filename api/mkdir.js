288f570a6d48f67a350c13297cfc15cd666b43f8
lirilabs <lirilabs@gmail.com>
2025-12-12 19:24:47 +0530

    Create mkdir.js

---
+++ api/mkdir.js
@@ -0,0 +1,11 @@
+import { githubPut } from "./utils";
+
+export default async function handler(req, res) {
+  const { folder } = req.body;
+
+  if (!folder) return res.status(400).json({ error: "folder required" });
+
+  const result = await githubPut(`${folder}/.gitkeep`, "", `Create folder ${folder}`);
+
+  res.status(200).json(result);
+}
