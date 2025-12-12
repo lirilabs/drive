1ec2e07b76037c40f599ae82d40de45a46e19f00
lirilabs <lirilabs@gmail.com>
2025-12-12 19:28:44 +0530

    Update encrypt.js

--- api/encrypt.js
+++ api/encrypt.js
@@ -4,12 +4,16 @@ export default async function handler(req, res) {
   const { data, filename } = req.body;
 
   if (!data || !filename)
-    return res.status(400).json({ error: "data + filename required" });
+    return res.status(400).json({ error: "data and filename required" });
 
   const encrypted = encryptAES(data);
   const content = JSON.stringify(encrypted, null, 2);
 
-  const result = await githubPut(`database/encrypted/${filename}`, content, "Encrypted Save");
+  const result = await githubPut(
+    `database/encrypted/${filename}`,
+    content,
+    "Encrypted Save"
+  );
 
   res.status(200).json(result);
 }
